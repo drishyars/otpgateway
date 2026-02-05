4fc907186ef4d8cf34ccc6be3db1683968b4727d
Drishya R <drishyarsd@gmail.com>
2026-02-05 15:54:43 +0530

    fix: return apt Gone status code and error message when OTP no longer exists in the store.

--- cmd/otpgateway/handlers_test.go
+++ cmd/otpgateway/handlers_test.go
@@ -234,15 +234,15 @@ func TestCheckOTP(t *testing.T) {
 
 	// Check it again. Should be deleted.
 	r = testRequest(t, http.MethodPost, "/api/otp/"+dummyOTPID, cp, &data)
-	assert.NotEqual(t, http.StatusOK, r.StatusCode, "OTP didn't get deleted on verification")
+	assert.Equal(t, http.StatusGone, r.StatusCode, "OTP didn't get deleted on verification")
 
-	// Check non-existent OTP, should not return 200.
+	// Check non-existent OTP, should return 410 Gone.
 	r = testRequest(t, http.MethodPost, "/api/otp/abc123", cp, &data)
-	assert.NotEqual(t, http.StatusOK, r.StatusCode, "non-existent OTP didn't return 400")
+	assert.Equal(t, http.StatusGone, r.StatusCode, "non-existent OTP didn't return 410")
 
 	// Check non-existent OTP, should return store.ErrNotExist error message.
 	_ = testRequest(t, http.MethodPost, "/api/otp/abc123", cp, &out)
-	assert.Equal(t, "the OTP does not exist", out.Message, "non-existent OTP passed")
+	assert.Equal(t, "OTP has expired or doesn't exist", out.Message, "non-existent OTP passed")
 }
 
 func TestCheckOTPAttempts(t *testing.T) {
